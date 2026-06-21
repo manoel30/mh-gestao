d5cc9cece178d7d4c2792c25c05ccc0058e84610
manoel30 <manoel@menendez.com.br>
2026-06-21 12:30:10 -0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from database import db
 class Fornecedor(db.Model):
     __tablename__ = 'fornecedores'
     id = db.Column(db.Integer, primary_key=True)
-    razao_social = db.Column(db.String(100), nullable=False)
+    razao_social = db.Column(db.String(200), nullable=False)
     cnpj = db.Column(db.String(18), unique=True, nullable=False)
     telefone = db.Column(db.String(20))
     email = db.Column(db.String(100))
